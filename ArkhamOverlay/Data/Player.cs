5152833627e6ee0898b6044ae38c58e08f167b76
slyjeff <sylvesterjj@gmail.com>
2021-01-18 11:15:10 -0500

    Added buttons for tracking stats for players

--- ArkhamOverlay/Data/Player.cs
+++ ArkhamOverlay/Data/Player.cs
@@ -1,45 +1,91 @@
-﻿using Newtonsoft.Json;
+﻿using PageController;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace ArkhamOverlay.Data {
-    public class Player : INotifyPropertyChanged {
+    public class Player : ViewModel {
         public Player(int id) {
             ID = id;
             SelectableCards = new SelectableCards(SelectableType.Player);
+            Health = new Stat("health.png");
+            Sanity = new Stat("sanity.png");
+            Resources = new Stat("resource.png");
+            Clues = new Stat("clue.png");
         }
 
         public int ID { get; }
 
         public string DeckId { get; set; }
 
-        [JsonIgnore]
         public SelectableCards SelectableCards { get; }
 
-        [JsonIgnore]
         public BitmapImage InvestigatorImage { get; set; }
 
-        [JsonIgnore]
         public IDictionary<string, int> Slots { get; set; }
 
-        [JsonIgnore]
         public Visibility LoadedVisiblity { get { return string.IsNullOrEmpty(SelectableCards.Name) ? Visibility.Hidden : Visibility.Visible; } }
 
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        public void OnPropertyChanged(string propertyName) {
-            var handler = PropertyChanged;
-            if (handler == null) {
-                return;
-            }
-            handler(this, new PropertyChangedEventArgs(propertyName));
+        public void OnPlayerChanged() {
+            NotifyPropertyChanged(nameof(LoadedVisiblity));
+            NotifyPropertyChanged(nameof(InvestigatorImage));
         }
 
-        public void OnPlayerChanged() {
-            OnPropertyChanged(nameof(LoadedVisiblity));
-            OnPropertyChanged(nameof(InvestigatorImage));
+        public Stat Health { get; }
+        public Stat Sanity { get; }
+        public Stat Resources { get; }
+        public Stat Clues { get; }
+    }
+
+    public class Stat : ViewModel {
+        public Stat(string imageFile) {
+            var fileName = AppDomain.CurrentDomain.BaseDirectory + "Images\\" + imageFile;
+            Image = new BitmapImage(new Uri(fileName));
+            Increase = new UpdateStateCommand(this, true);
+            Decrease = new UpdateStateCommand(this, false);
+        }
+
+        public ImageSource Image { get; }
+
+        private int _value;
+        public int Value { 
+            get => _value;
+            set {
+                _value = value;
+                NotifyPropertyChanged(nameof(Value));
+            }
+        }
+
+        public ICommand Increase { get; }
+        public ICommand Decrease { get; }
+    }
+
+    public class UpdateStateCommand : ICommand {
+        private Stat _stat;
+        private bool _increase;
+        public UpdateStateCommand(Stat stat, bool increase) {
+            _stat = stat;
+            _increase = increase;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter) {
+            return true;
+        }
+
+        public void Execute(object parameter) {
+            if (_increase) {
+                _stat.Value++;
+                return;
+            }
+
+            if (_stat.Value > 0) {
+                _stat.Value--;
+            } 
         }
     }
 }
