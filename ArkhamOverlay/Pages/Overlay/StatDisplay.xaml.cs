ca1386acbb449881758c44de07bf8f5ef0e252b9
slyjeff <sylvesterjj@gmail.com>
2021-01-19 19:35:15 -0500

    Added right alignment for stats on the right side of the page

--- ArkhamOverlay/Pages/Overlay/StatDisplay.xaml.cs
+++ ArkhamOverlay/Pages/Overlay/StatDisplay.xaml.cs
@@ -1,9 +1,12 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace ArkhamOverlay.Pages.Overlay {
     public partial class StatDisplay : Border {
         public StatDisplay() {
             InitializeComponent();
         }
+
+        public HorizontalAlignment ImageAlignment { get => Images.HorizontalAlignment; set => Images.HorizontalAlignment = value; }
     }
 }
