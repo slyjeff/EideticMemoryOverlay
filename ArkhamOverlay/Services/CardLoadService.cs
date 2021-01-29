0ed480c53fa6d735f10828e9b6f82a4ce303dc9d
slyjeff <sylvesterjj@gmail.com>
2021-01-29 09:07:22 -0500

    Moved logic adding cards to game data out of arkhamdb service and created service for loading local cards

--- ArkhamOverlay/Services/CardLoadService.cs
+++ ArkhamOverlay/Services/CardLoadService.cs
@@ -1,5 +1,6 @@
 ﻿using ArkhamOverlay.Data;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 
 namespace ArkhamOverlay.Services {
@@ -7,10 +8,12 @@ namespace ArkhamOverlay.Services {
         private readonly AppData _appData;
         private readonly LoadingStatusService _loadingStatusService;
         private readonly ArkhamDbService _arkhamDbService;
+        private readonly LocalCardsService _localCardsService;
         private readonly LoggingService _logger;
 
-        public CardLoadService(ArkhamDbService arkhamDbService, AppData appData, LoadingStatusService loadingStatusService, LoggingService loggingService) {
+        public CardLoadService(ArkhamDbService arkhamDbService, LocalCardsService localCardsService, AppData appData, LoadingStatusService loadingStatusService, LoggingService loggingService) {
             _arkhamDbService = arkhamDbService;
+            _localCardsService = localCardsService;
             _appData = appData;
             _loadingStatusService = loadingStatusService;
             _logger = loggingService;
@@ -25,14 +28,63 @@ namespace ArkhamOverlay.Services {
             var worker = new BackgroundWorker();
             worker.DoWork += (x, y) => {
                 _loadingStatusService.ReportEncounterCardsStatus(Status.LoadingCards);
+                _logger.LogMessage("Loading encounter cards.");
                 try {
-                    _arkhamDbService.LoadEncounterCards(_appData);
+                    _appData.Game.ScenarioCards.Loading = true;
+                    _appData.Game.LocationCards.Loading = true;
+                    _appData.Game.EncounterDeckCards.Loading = true;
+
+                    var cards = _arkhamDbService.LoadEncounterCards();
+                    cards.AddRange(_localCardsService.LoadEncounterCards());
+
                     _loadingStatusService.ReportEncounterCardsStatus(Status.Finished);
-                }
-                catch (Exception ex) {
+
+                    var scenarioCards = new List<Card>();
+                    var agendas = new List<Card>();
+                    var acts = new List<Card>();
+                    var locations = new List<Card>();
+                    var treacheries = new List<Card>();
+                    var enemies = new List<Card>();
+
+                    foreach (var card in cards) {
+                        switch (card.Type) {
+                            case CardType.Scenario:
+                                scenarioCards.Add(card);
+                                break;
+                            case CardType.Agenda:
+                                agendas.Add(card);
+                                break;
+                            case CardType.Act:
+                                acts.Add(card);
+                                break;
+                            case CardType.Location:
+                                locations.Add(card);
+                                break;
+                            case CardType.Treachery:
+                            case CardType.Enemy:
+                                treacheries.Add(card);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+
+                    scenarioCards.AddRange(agendas);
+                    scenarioCards.AddRange(acts);
+
+                    _appData.Game.ScenarioCards.LoadCards(scenarioCards);
+                    _appData.Game.LocationCards.LoadCards(locations);
+                    _appData.Game.EncounterDeckCards.LoadCards(treacheries);
+                } catch (Exception ex) {
                     _logger.LogException(ex, "Error loading encounter cards.");
                     _loadingStatusService.ReportEncounterCardsStatus(Status.Error);
+                } finally {
+                    _appData.Game.ScenarioCards.Loading = false;
+                    _appData.Game.LocationCards.Loading = false;
+                    _appData.Game.EncounterDeckCards.Loading = false;
                 }
+                _logger.LogMessage($"Finished loading encounter cards.");
+
             };
             worker.RunWorkerAsync();
         }
---
+++ ArkhamOverlay/Services/LocalCardsService.cs
@@ -0,0 +1,10 @@
+﻿using ArkhamOverlay.Data;
+using System.Collections.Generic;
+
+namespace ArkhamOverlay.Services {
+    public class LocalCardsService {
+        public List<Card> LoadEncounterCards() {
+            return new List<Card>();
+        }
+    }
+}
