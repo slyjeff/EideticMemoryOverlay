d7afef410675f14bb97f0e1c19ac722f049353e5
slyjeff <sylvesterjj@gmail.com>
2021-02-22 07:49:44 -0500

    Removed commented code

--- ArkhamOverlay/CardButtons/IHasImageButton.cs
+++ ArkhamOverlay/CardButtons/IHasImageButton.cs
@@ -3,7 +3,6 @@
 namespace ArkhamOverlay.CardButtons {
     public interface IHasImageButton {
         string Name { get; }
-        //string Code { get; }
         string ImageId { get; }
         CardType ImageCardType { get; }
         string ImageSource { get; set; }
