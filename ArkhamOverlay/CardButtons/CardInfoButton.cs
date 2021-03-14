7e50094ae5820d92be3efdad836199abeb88700f
slyjeff <sylvesterjj@gmail.com>
2021-03-14 09:52:54 -0400

    Made ZoneIndex a named parameter for Card Info Button

--- ArkhamOverlay/CardButtons/CardInfoButton.cs
+++ ArkhamOverlay/CardButtons/CardInfoButton.cs
@@ -6,25 +6,25 @@ namespace ArkhamOverlay.CardButtons {
     public class CardInfoButton : CardImageButton {
         public CardInfoButton(CardInfo cardInfo, CardGroupId cardGroupId) : base(cardInfo, false) {
             if (cardInfo.IsPlayerCard) {
-                Options.Add(new ButtonOption(ButtonOptionOperation.Add, cardGroupId, 0));
+                Options.Add(new ButtonOption(ButtonOptionOperation.Add, cardGroupId, zoneIndex: 0));
             }
 
             if (cardInfo.Type == CardType.Act || cardInfo.Type == CardType.Agenda) {
-                Options.Add(new ButtonOption(ButtonOptionOperation.Add, cardGroupId, 0));
+                Options.Add(new ButtonOption(ButtonOptionOperation.Add, cardGroupId, zoneIndex: 0));
             }
 
             if (!cardInfo.IsPlayerCard && cardInfo.IsHidden) {
-                Options.Add(new ButtonOption(ButtonOptionOperation.Add, CardGroupId.Player1, 0));
-                Options.Add(new ButtonOption(ButtonOptionOperation.Add, CardGroupId.Player2, 0));
-                Options.Add(new ButtonOption(ButtonOptionOperation.Add, CardGroupId.Player3, 0));
-                Options.Add(new ButtonOption(ButtonOptionOperation.Add, CardGroupId.Player4, 0));
+                Options.Add(new ButtonOption(ButtonOptionOperation.Add, CardGroupId.Player1, zoneIndex: 0));
+                Options.Add(new ButtonOption(ButtonOptionOperation.Add, CardGroupId.Player2, zoneIndex: 0));
+                Options.Add(new ButtonOption(ButtonOptionOperation.Add, CardGroupId.Player3, zoneIndex: 0));
+                Options.Add(new ButtonOption(ButtonOptionOperation.Add, CardGroupId.Player4, zoneIndex: 0));
             }
 
             if (!cardInfo.IsHidden && cardInfo.Type == CardType.Treachery || cardInfo.Type == CardType.Enemy || cardInfo.Type == CardType.Location) {
-                Options.Add(new ButtonOption(ButtonOptionOperation.Add, CardGroupId.Player1, 1));
-                Options.Add(new ButtonOption(ButtonOptionOperation.Add, CardGroupId.Player2, 1));
-                Options.Add(new ButtonOption(ButtonOptionOperation.Add, CardGroupId.Player3, 1));
-                Options.Add(new ButtonOption(ButtonOptionOperation.Add, CardGroupId.Player4, 1));
+                Options.Add(new ButtonOption(ButtonOptionOperation.Add, CardGroupId.Player1, zoneIndex: 1));
+                Options.Add(new ButtonOption(ButtonOptionOperation.Add, CardGroupId.Player2, zoneIndex: 1));
+                Options.Add(new ButtonOption(ButtonOptionOperation.Add, CardGroupId.Player3, zoneIndex: 1));
+                Options.Add(new ButtonOption(ButtonOptionOperation.Add, CardGroupId.Player4, zoneIndex: 1));
             }
         }
     }
